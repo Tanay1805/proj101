1e8b500f9235e0ffa0f040136565c80708f850eb
anusrimk <142775407+anusrimk@users.noreply.github.com>
2023-10-03 16:22:26 +0530

    Update 36_rightangletri.cpp

--- 36_rightangletri.cpp
+++ 36_rightangletri.cpp
@@ -1,3 +1,4 @@
+//program that prints the pattern of a right-angled triangle using nested loops.
 #include <iostream>
 using namespace std;
 
@@ -5,11 +6,11 @@ int main() {
 
     int rows;
 
-    cout << "Enter number of rows: ";
+    cout << "Enter number of rows: "; // taking input
     cin >> rows;
 
-    for(int i = 1; i <= rows; ++i) {
-        for(int j = 1; j <= i; ++j) {
+    for(int i = 1; i <= rows; ++i) {  // to run program till rows
+        for(int j = 1; j <= i; ++j) {   // for printing rows in the right angle program
             cout << "* ";
         }
         cout << "\n";
