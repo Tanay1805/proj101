fd6a85333d83d188abbf8cf7a7bce54416a42858
anusrimk <142775407+anusrimk@users.noreply.github.com>
2023-10-03 17:20:27 +0530

    Update 89_Printing_Pattern.cpp

--- 89_Printing_Pattern.cpp
+++ 89_Printing_Pattern.cpp
@@ -9,19 +9,19 @@ using namespace std;
 
 int main(){
 
-    int i,j,row;
+    int i,j,row;  // declaring variable rows
 
     cout<<("Enter number of rows: ");
     cin>>row;
 
-    for(i=1;i<=row;i++)
+    for(i=1;i<=row;i++)   // for prinitng till row values
     {
         for(j=i;j>=1;j--)
         {
             cout<<j;
         }
 
-        for(j=2;j<=i;j++)
+        for(j=2;j<=i;j++)  // for horizontally and vertically printing
         {
             cout<<j;
         }
