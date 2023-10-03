0cde84174e1af4d765261ad8ba4ce743d5d0317a
anusrimk <142775407+anusrimk@users.noreply.github.com>
2023-10-03 16:30:44 +0530

    Update 57_removeduplicate.cpp

--- 57_removeduplicate.cpp
+++ 57_removeduplicate.cpp
@@ -2,16 +2,20 @@
 #include<iostream>
 using namespace std;
 int main ()
+
+
 {
+
+    //declaring the array size and variables
     int A[10], B[10], n, i, j, k = 0;
     cout << "Enter size of array : ";
     cin >> n;
 
     cout << "Enter elements of array : ";
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++) // for running the statement will nth element
         cin >> A[i];  
           
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++) //to check whether the elements are the same
     {
         for (j = 0; j < k; j++)
         {
@@ -25,7 +29,7 @@ int main ()
         }
     }
     cout << "Repeated elements after deletion : ";
-    for (i = 0; i < k; i++)
+    for (i = 0; i < k; i++)  // for displaying the elements after duplication ends
         cout << B[i] << " ";
     return 0;
 }
