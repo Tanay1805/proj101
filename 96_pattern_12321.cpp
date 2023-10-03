68bcb9d64543ab611e3888a49575dd49985a9fa6
anusrimk <142775407+anusrimk@users.noreply.github.com>
2023-10-03 17:23:16 +0530

    Update 96_pattern_12321.cpp

--- 96_pattern_12321.cpp
+++ 96_pattern_12321.cpp
@@ -1,3 +1,10 @@
+/*"Create a C++ program to print the following pattern:
+
+    1
+   121
+  12321
+ 1234321
+123454321"*/
 #include <iostream>  
 using namespace std;  
 int main()  
@@ -7,15 +14,12 @@ int main()
     cin>>n;    
     for(i=1;i<=n;i++)    
     {
-        //for(j=1;j<=n-i;j++)    
-        //{
-        //    cout<<" ";    
-        //}    
-    for(k=1;k<=i;k++)    
+        
+    for(k=1;k<=i;k++)    //for incrementing values
     {  
         cout<<k;    
     }    
-    for(l=i-1;l>=1;l--)    
+    for(l=i-1;l>=1;l--)  // for generating the vnumbers backwards  
     {
         cout<<l;    
     }    
