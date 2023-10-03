0468ffb67527fa329f732ec856a946844fea3c46
anusrimk <142775407+anusrimk@users.noreply.github.com>
2023-10-03 15:33:34 +0530

    Update 12_leapyear.cpp

--- 12_leapyear.cpp
+++ 12_leapyear.cpp
@@ -1,40 +1,3 @@
-/*// Program to convert temperature from Celsius to Fahrenheit and vice versa.
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n;
-    float Celsius;
-    float Fahrenheit;
-
-    cout<< "Do you want to convert (1-Celsius to F / 2-Fahrenheit to C):  "; // users can have option to chose(1/2) 
-    cin>>n;
-     if(n==1){
-        cout<<"Enter Celsius (in Degrees): ";
-        cin>>Celsius;
-
-        Fahrenheit = (1.8 * Celsius) + 32;   // formula for celsius to degree
-        cout<<"The temperature in Fahrenheit is "<< Fahrenheit<<" F";
-     }
-     else{
-        cout<<"Enter Fahrenheit (in Degre F): ";
-        cin>>Fahrenheit;
-
-        Celsius = (Fahrenheit - 32) / 32;  // formula for fahrenheit to celsius
-        cout<<"The temperature in Celsius is "<<Celsius<<" C";
-     }    
-
-    return 0;
-}
-*/
-/*// not a leap year if divisible by 100
-  // but not divisible by 400
-  else if (year % 100 == 0) {
-    cout << year << " is not a leap year.";*/
-  
-  // leap year if not divisible by 100
-  // but divisible by 4
-
 #include <iostream>
 using namespace std;
 
@@ -44,7 +7,8 @@ int main() {
   cout << "Enter a year: ";
   cin >> year;
 
-  // leap year if perfectly divisible by 400
+  // leap year if perfectly divisible by 400 also if the year is the year is divisble by 100
+  // because if year is divisible by 100 it is not a leap year
   if (year % 400 == 0 && year % 100 == 0) {
     cout << year << " is a leap year.";
   }
