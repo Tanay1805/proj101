6ea18aef5b5d922fd84e00c83fa64b3bdaf1bec2
Yashika1311 <142776188+Yashika1311@users.noreply.github.com>
2023-10-04 10:56:25 +0530

    Update prog_100.cpp
    c

--- prog_100.cpp
+++ prog_100.cpp
@@ -4,13 +4,13 @@ int main()
 {
  
   int i,j;
-  int n=5;
+  int n=5;//represents no of rows in pattern 
  
-   for(i=n;i>=1;i--)
+   for(i=n;i>=1;i--) //begins with 5 and continues till i as 1 
      {
-         for(j=i;j<=n;j++)
+         for(j=i;j<=n;j++) // this is for how many letters to be printed on each row
          {
-             cout<<((char)(j+64));
+             cout<<((char)(j+64));//  here we take j=1 so 1+64=65 ie A
          }
  
          cout<<endl;
